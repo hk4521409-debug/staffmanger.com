f4e57b9b7caf7bac995cb09c74317a7d41dad9d7
hk4521409-debug <hk4521409@gmail.com>
2026-02-22 11:21:54 +0530

    Create Announcement.js

---
+++ Announcement.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const announcementSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  targetRoles: [{ type: String, enum: ['admin', 'faculty', 'student'] }],
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Announcement', announcementSchema);
